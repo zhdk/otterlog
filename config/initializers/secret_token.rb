6f99669b7dcd2d4bc9617dd018393dc33b4b97e9
Ramón Cahenzli <rca@psy-q.ch>
2012-10-25 14:55:28 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Otterlog::Application.config.secret_token = '66dfe2a767bcdf3972c50d7f0cac54cbb7e2bb891496b3fbf19ca7fa8c2fe1c0d546664ec52174ecf87def2063e148f2c5c63a13ef6084c86a981dd2a9d6beb7'
